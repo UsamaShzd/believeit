21ea41dafd5813d896ac102070139655e46a8bcb
Usama Shehzad <usamashzd99@gmail.com>
2021-05-18 14:07:39 +0500

    saved post bug removed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -55,7 +55,7 @@ router.get("/saved_posts", authorize(), async (req, res) => {
     },
   ]);
 
-  const result = savedPost.map((p) => {
+  const result = savedPosts.map((p) => {
     return { ..._.pick(p.post, postFields), saved: true };
   });
   res.send(result);
