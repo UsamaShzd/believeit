61ffdd9470de1937f2150503e3b1796429109e6f
Usama Shehzad <usamashzd99@gmail.com>
2021-07-13 15:34:52 +0500

    feedback message fixed

--- validators/feedback.js
+++ validators/feedback.js
@@ -4,7 +4,7 @@ const feedBackSchema = yup.object().shape({
   name: yup.string().min(0).max(200).required(),
   email: yup.string().email().min(0).max(200).required(),
   subject: yup.string().min(0).max(200).required(),
-  message: yup.string().email().min(0).max(200).required(),
+  message: yup.string().min(0).max(200).required(),
 });
 
 module.exports = {
