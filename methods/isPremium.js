1ad4efb722637188be6c52eef6cc2fe143c71ed1
Usama Shehzad <usama.shehzad@virusgeeks.com>
2021-07-30 20:00:38 +0500

    isPremium function improved

--- methods/isPremium.js
+++ methods/isPremium.js
@@ -2,11 +2,14 @@ const { FREE } = require("../enums/subscription_plans");
 
 module.exports = (subscription) => {
   if (subscription.type === FREE.name) return false;
+  try {
+    const currentDate = new Date();
+    const subscriptionEndDate = new Date(subscription.endDate);
 
-  const currentDate = new Date();
-  const subscriptionEndDate = new Date(subscription.endDate);
+    if (subscriptionEndDate < currentDate) return false;
 
-  if (subscriptionEndDate < currentDate) return false;
-
-  return true;
+    return true;
+  } catch (err) {
+    return false;
+  }
 };
