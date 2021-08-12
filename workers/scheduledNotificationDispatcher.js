63e2eca3c5da6312ba6344f3deeadd457824ff50
Usama Shehzad <usama.shehzad@virusgeeks.com>
2021-08-12 17:44:42 +0500

    push notification type bug fixed

--- workers/scheduledNotificationDispatcher.js
+++ workers/scheduledNotificationDispatcher.js
@@ -119,8 +119,8 @@ module.exports = async () => {
           "Work on your goal plans, complete today's Milestones and say Affirmations.";
         break;
     }
-
-    push_notification.body.type = type;
+    if(!push_notification.data) push_notification.data = {}
+    push_notification.data.type = type;
     sendPushNotifications(push_notification);
   });
 };
