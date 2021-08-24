395bba0c6fb1a5a30c9d29950e6319e04ec04d29
Usama Shehzad <usama.shehzad@virusgeeks.com>
2021-08-24 18:04:10 +0500

    send feedback email implemented

---
+++ helpers/sendFeedbackEmail.js
@@ -0,0 +1,24 @@
+const mailer = require("../services/mailer");
+
+module.exports = async ({
+  name = "",
+  email = "",
+  subject = "",
+  message = "",
+}) => {
+  mailer.sendMail({
+    to: `${feedback.email}, ${process.env.SMTP_EMAIL}`,
+    subject, // Subject line
+    text: "Feedback Email", // plain text body
+    template: "email/verify_email",
+    context: {
+      layout: "email",
+
+      heading: "Feedback From " + name,
+      email,
+      message,
+
+      logo: process.env.BASE_URL + "/images/email_logo.png",
+    },
+  });
+};
--- routes/api/feedback.js
+++ routes/api/feedback.js
@@ -3,6 +3,7 @@ const authorize = require("../../middlewares/authorize");
 const requestValidator = require("../../middlewares/requestValidator");
 
 const { feedBackSchema } = require("../../validators/feedback");
+const sendFeedbackEmail = require("../../helpers/sendFeedbackEmail");
 const router = express.Router();
 
 router.post(
@@ -12,8 +13,27 @@ router.post(
   async (req, res) => {
     //TODO: send email
 
+    sendFeedbackEmail(req.body);
     res.send({ message: "Feedback Email sent" });
   }
 );
+
+router.get("/", (req, res) => {
+  const feedback = {
+    name: "Usama Shehzad",
+    email: "UsamaShzd99@gmail.com",
+    subject: "Feedback Email",
+    message: "Feedback Message",
+  };
+
+  const { name, email, subject, message } = feedback;
+  res.render("email/feedback", {
+    layout: "email",
+    heading: "Feedback From " + name,
+    email,
+    message,
+    logo: process.env.BASE_URL + "/images/email_logo.png",
+  });
+});
 
 module.exports = router;
