3a9e9bceb463351a99cccd95058ce855be688c59
Usama Shehzad <usama.shehzad@virusgeeks.com>
2021-08-24 18:12:57 +0500

    feedback template rendering route removed

--- routes/api/feedback.js
+++ routes/api/feedback.js
@@ -12,28 +12,9 @@ router.post(
   authorize(),
   async (req, res) => {
     //TODO: send email
-
     sendFeedbackEmail(req.body);
     res.send({ message: "Feedback Email sent" });
   }
 );
-
-router.get("/", (req, res) => {
-  const feedback = {
-    name: "Usama Shehzad",
-    email: "UsamaShzd99@gmail.com",
-    subject: "Feedback Email",
-    message: "Feedback Message",
-  };
-
-  const { name, email, subject, message } = feedback;
-  res.render("email/feedback", {
-    layout: "email",
-    heading: "Feedback From " + name,
-    email,
-    message,
-    logo: process.env.BASE_URL + "/images/email_logo.png",
-  });
-});
 
 module.exports = router;
