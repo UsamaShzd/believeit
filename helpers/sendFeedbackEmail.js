3a8d1778b878d72f49bba904c4c7ef80b18e534a
Usama Shehzad <usama.shehzad@virusgeeks.com>
2021-09-06 19:02:52 +0500

    Feedback email fixed

--- helpers/sendFeedbackEmail.js
+++ helpers/sendFeedbackEmail.js
@@ -10,14 +10,12 @@ module.exports = async ({
     to: `${email}, ${process.env.SMTP_EMAIL}`,
     subject, // Subject line
     text: "Feedback Email", // plain text body
-    template: "email/verify_email",
+    template: "email/feedback",
     context: {
       layout: "email",
-
       heading: "Feedback From " + name,
       email,
       message,
-
       logo: process.env.BASE_URL + "/images/email_logo.png",
     },
   });
